a8d4d08efd8b5fdf92023942bfd25e15d7fae8cc
aktsu <black_atsku@proton.me>
2023-11-30 23:03:10 -0300

    nvim config att

--- dotfiles/nvim/lua/aktsu/plugins-setup.lua
+++ dotfiles/nvim/lua/aktsu/plugins-setup.lua
@@ -16,7 +16,7 @@ require("lazy").setup({{
   lazy = false,
   priority = 1000,
   opts = {},
-  }, 
+  },
   "christoomey/vim-tmux-navigator",  --windows navigation and tmux integration    
   "szw/vim-maximizer",  --maximize and restore current window
   "numToStr/Comment.nvim", --better commenting
@@ -24,6 +24,48 @@ require("lazy").setup({{
   "nvim-tree/nvim-tree.lua", -- file explorer
   "kyazdani42/nvim-web-devicons", --pretty icons
   "nvim-lualine/lualine.nvim", -- status bar
+  -- fuzzy finding w/ telescope
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }, -- dependency for better sorting performance
+  { "nvim-telescope/telescope.nvim", branch = "0.1.x" }, -- fuzzy finder
+
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
+    run = function()
+      local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+      ts_update()
+    end,
+  },  --to better highlight
+    -- autocompletion
+  "hrsh7th/nvim-cmp", -- completion plugin
+  "hrsh7th/cmp-buffer", -- source for text in buffer
+  "hrsh7th/cmp-path", -- source for file system paths
+
+    -- snippets
+  "L3MON4D3/LuaSnip", -- snippet engine
+  "saadparwaiz1/cmp_luasnip", -- for autocompletion
+  "rafamadriz/friendly-snippets", -- useful snippets
+
+   -- managing & installing lsp servers, linters & formatters
+  "williamboman/mason.nvim", -- in charge of managing lsp servers, linters & formatters
+  "williamboman/mason-lspconfig.nvim", -- bridges gap b/w mason & lspconfig
+
+  -- configuring lsp servers
+  "neovim/nvim-lspconfig", -- easily configure language servers
+  "hrsh7th/cmp-nvim-lsp", -- for autocompletion
+  { "glepnir/lspsaga.nvim",
+    branch = "main",
+    requires = {
+      { "nvim-tree/nvim-web-devicons" },         --enhanced lsp uis 
+      { "nvim-treesitter/nvim-treesitter" },
+    },
+  },
+  "onsails/lspkind.nvim", -- vs-code like icons for autocompletion
+
+  -- auto closing
+  "windwp/nvim-autopairs", -- autoclose parens, brackets, quotes, etc...
+
+  -- git integration
+  "lewis6991/gitsigns.nvim", -- show line modifications on left hand side
+
   { "nvimdev/dashboard-nvim",
   event = "VimEnter",
   config = function()
@@ -32,7 +74,7 @@ require("lazy").setup({{
 			config = {
 			  week_header = {
 			      enable = true,
-			      concat = "Shishiro Botan (獅白ぼたん)"
+			      concat = "Henya dayo! (へにゃです)"
 			},
 			footer = {},
 			shortcut = {
---
+++ dotfiles/nvim/lua/aktsu/plugins/lsp/mason.lua
@@ -0,0 +1,26 @@
+-- import mason plugin safely
+local mason_status, mason = pcall(require, "mason")
+if not mason_status then
+  return
+end
+
+-- import mason-lspconfig plugin safely
+local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not mason_lspconfig_status then
+  return
+end
+
+-- enable mason
+mason.setup()
+
+mason_lspconfig.setup({
+  -- list of servers for mason to install
+  ensure_installed = {
+    "clangd",                         --C/C++
+    "julials",                        --julia
+    "ltex",                           --latex
+    "lua_ls",                         --lua
+    "marksman",                       --markdown
+    "jedi_language_server",           --python
+    "rust_analyzer",                  --rust
+  }})
