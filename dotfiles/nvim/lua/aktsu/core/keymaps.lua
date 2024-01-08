20dcd75104be6dc29cdd5a1fd4ba3e164e9d41cf
aktsu <black_atsku@proton.me>
2024-01-08 15:56:57 -0300

    att config

--- dotfiles/nvim/lua/aktsu/core/keymaps.lua
+++ dotfiles/nvim/lua/aktsu/core/keymaps.lua
@@ -30,3 +30,7 @@ keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in 
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+
+-- Package manager keymaps
+-- keymap.set("n", "<leader>l", "<cmd>:Lazy<cr>", { desc = " [L]azy package manager" })
+keymap.set("n", "<leader>m", "<cmd>:Mason<cr>", { desc = " [M]ason LSP manager" })
